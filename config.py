09f8b606677fd8ca737ebcf6c0c534bd872bda45
Aakarsh Kumar <aakarsh2504@gmail.com>
2024-12-30 19:34:29 +0530

    modification: added config file-.env

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+class Config:
+    load_dotenv() 
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
